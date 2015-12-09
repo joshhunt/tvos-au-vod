a77558718af3a252f346c459a72c5476a5009a0a
Josh Hunt <josh@trtr.co>
2015-12-10 00:42:33 +1100

    Load iView homepage

--- client/src/views/home/index.js
+++ client/src/views/home/index.js
@@ -1,6 +1,12 @@
 import homeTemplate from './template.tvml.jade';
+import axios from 'axios';
+
+const endpoint = 'https://iview.abc.net.au/api/home?device=ios-appletv&appver=dev';
 
 export default function homeView(ctx) {
-  const doc = homeTemplate();
-  ctx.present(doc);
+  axios.get(endpoint)
+    .then(({ data }) => {
+      const doc = homeTemplate(data);
+      ctx.present(doc);
+    });
 }
